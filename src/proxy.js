0765c9c2c210f11264e66966e257b8564838b4b6
jimous <jimouspeng@xiaoe-tech.com>
2022-02-17 13:31:53 +0800

    fix: proxy---end

--- src/proxy.js
+++ src/proxy.js
@@ -2,8 +2,8 @@
 /*
  * @Date: 2022-02-16 10:00:12
  * @LastEditors: jimouspeng
- * @Description: ES6-Proxy代理
- * @LastEditTime: 2022-02-16 14:24:09
+ * @Description: ES6-Proxy代理【与代理相关的操作性能还不够理想，优化空间有限，代理有可能将问题复杂化，所以谨慎使用】
+ * @LastEditTime: 2022-02-17 13:30:40
  * @FilePath: \es6\src\proxy.js
  */
 console.log('proxy----------------------------------------------------------------------------------------start')
@@ -89,6 +89,65 @@ function proxied(target, flag = '_') {
                 return true
             })
         },
+        /**
+         * getOwnPropertyDescriptor捕获器，查询对象某个属性描述符触发
+         * 如果属性存在，返回该属性的描述符，否则返回undefined，也可以自定义抛出异常
+         * 调用Object.getOwnPropertyDescriptor || .hasOwnProperty 触发
+         */
+        getOwnPropertyDescriptor(target, key) {
+            console.log('触发getOwnPropertyDescripto');
+            checkKey(key, 'getOwnPropertyDescriptor---非法')
+            return Reflect.getOwnPropertyDescriptor(target, key)
+        },
+        /**
+         * apply捕获器 当代理的target函数被调用时触发
+         * target: 被代理的函数
+         * ctx: 函数被调用时作为target中的this的上下文对象
+         * args: 函数被调用时传给target的数组
+         * 除了能够获取每次通过proxy调用函数时的参数，还可以用于添加额外的参数或修改函数调用的结果
+         * 所有这些都可以在不改变底层target函数的前提下实现。
+         */
+        apply(target, ctx, args) {
+            return Reflect.apply(target, ctx, args)
+        },
+        /** construct捕获器：拦截 new 操作符的调用
+         * 常见用例： 重组构造器的参数或者做一些本应有构造器来做的事
+         */
+        construct(Target, args) {
+            // return new Target(...args) 等同于下面
+            return Reflect.construct(Target, args)
+        },
+        /** getPrototypeOf捕获器用于拦截以下所有操作
+         * 访问Object.__proto__属性
+         * 调用Object.isPrototypeOf
+         * 调用Object.getPrototypeOf
+         * 调用Reflect.getPrototypeOf
+         * 使用instanceof操作符
+         * 利用该捕获器可以动态地指定要报告的底层对象的原型
+         */
+        getPrototypeOf() {
+            return Array.prototype // 让某个对象在通过代理访问时被当成Array
+            // return xxx.prototype
+        },
+        /** setPrototypeOf捕获器：修改对象的原型 */
+        setPrototypeOf(target, proto) {
+            Object.setPrototypeOf(target, proto);
+            // Reflect.setPrototypeOf(target, proto); // 跟上面方法等价
+            // 也可以直接抛出错误，显示的禁止修改原型
+        },
+        /** 对象新增属性捕获器，捕获ES5新增的Object.preventExtensions方法 */
+        preventExtensions(target) {
+            return Reflect.preventExtensions(target)
+        },
+        /** isExtensible捕获器用来记录或监督对Object.isExtensible的调用，但不能左右对象是否可以扩展的试试。
+         * 仅有监督的作用，如果不想让用户知道底层对象是否可以扩展，可以抛出错误
+         */
+        isExtensible()
+    }
+    function checkKey(key, str) {
+        if(key.startsWith(flag)) {
+            throw new Error(str)
+        }
     }
     return new Proxy(target, handler)
 }
@@ -125,6 +184,7 @@ console.log(proxy1.jimous, proxy1.somethingElse) // jimous, undefined (触发han
 try {
     // console.log(proxy1._test)
     console.log('_jimous' in proxy1) // false
+    // console.log(Object.getOwnPropertyDescriptor(proxy1, '_jimous'));
     console.log(proxy1.hasOwnProperty('_jimous')) // true
 } catch (err) {
     console.log(err)
@@ -170,5 +230,89 @@ function revokeStorage() {
 // pizza.topping = 'ham'
 // Object.defineProperty(pizza, 'extraCheese', { value: true })
 // console.log(pizza, Object.getOwnPropertyDescriptor(pizza, 'topping'), Object.getOwnPropertyDescriptor(pizza, 'extraCheese'))
+
+
+/**apply捕获器实践 */
+const twice = {
+    apply(target, ctx, args) {
+        return Reflect.apply(target, ctx, args) * 2
+    }
+}
+function sum(a, b) {
+    return a + b;
+}
+
+const proxy2 = new Proxy(sum, twice);
+console.log(proxy2(1,2), '函数代理打印'); // 打印6
+/**案例
+ * 如果要保证每次调用test都返回logger对象,那么就需要将test与logger绑定，利用bind函数特性
+ */
+const logger = {
+    test() {
+        return this
+    },
+    test1() {
+        return this
+    }
+}
+// logger.test = logger.test.bind(logger);
+// logger.test1 = logger.test1.bind(logger);
+// ...如果有多个函数，那么每个函数都必要通过bind进行上下文绑定, 并且上下文依然会被call(...)劫持
+// 不如借助代理试试，以下实现容易导致 logger.test !== logger.test, 因为proxy返回了一个全新的绑定版函数
+// 解决思路： 利用 weakMap，见function selfish2
+const selfish = {
+    get(target, key) {
+        const value = Reflect.get(target, key);
+        if(typeof value !== 'function') {
+            return value;
+        }
+        return value.bind(target)
+    }
+}
+// 实现如下
+const proxyFun = new Proxy(logger, selfish)
+
+/** 改良版 */
+function selfish2(target) {
+    const cache = new WeakMap();
+    const handler = {
+        get(target, key) {
+            const value = Reflect.get(target, key);
+            if(typeof value !== 'function') {
+                return value;
+            }
+            if(!cache.has(value)) {
+                cache.set(value, value.bind(target))
+            }
+            return cache.get(value)
+        }
+    }
+    return new Proxy(target, handler)
+}
+
+/** construct捕获器案例 */
+// 假如有以下代码
+class Jimous {
+    hello() {
+        console.log(`hello, ${this.name}`);
+    }
+}
+const jimous1 = new Jimous();
+jimous1.name = 'jimous111';
+jimous1.hello(); // hello, jimous111
+const handleName = {
+    construct(Target, args) {
+        const [name] = args;
+        const target = Reflect.construct(Target, args);
+        target.name = name;
+        return target
+    }
+}
+// 通过代理，就不用每次都需要实例化之后再对name属性赋值了
+const JimousProxy = new Proxy(Jimous, handleName);
+const jimous2 = new JimousProxy('jimous222')
+const jimous3 = new JimousProxy('jimous333')
+jimous2.hello()
+jimous3.hello()
 
 console.log('proxy----------------------------------------------------------------------------------------end')
