288cbbd9b6a1dd2c84dd4f3131c59f77917d67ed
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-05-26 16:37:14 +0800

    fix: 重温下结构

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@
 
 // getUserInfo();
 
-const { name, age = 12 } = {};
-console.log(name, age);
+// const { name, age = 12 } = {};
+// console.log(name, age);
+
+const originObj = {
+    name: 'jimous',
+    age: 25,
+    schoolInfo: {
+        // name: 'ncu',
+        city: 'nanchang',
+    },
+};
+
+const {
+    name,
+    schoolInfo: { city: schoolCity, style = '211', name: schoolName = 'ncu' },
+} = originObj;
+console.log('name: ', name);
+console.log('city: ', schoolCity);
+console.log('style: ', style);
+console.log('schoolName: ', schoolName);
